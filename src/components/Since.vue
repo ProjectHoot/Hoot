26d7539d84c684c6c77a6d9fe0bcf7d59c20aa60
squash <josh@shortcutsolutions.net>
2021-08-04 17:45:15 -0400

    Revert Since.vue to compute time since rather than use external package

--- src/components/Since.vue
+++ src/components/Since.vue
@@ -1,5 +1,5 @@
 <template>
-    <span> {{ Timestamp | moment("from") }}</span>
+    <span> {{ timeSince }}</span>
 </template>
 
 <script>
@@ -8,5 +8,21 @@
     props: {
         Timestamp: String,
     },
+      computed: {
+        timeSince: function() {
+          var ts=Date.parse(this.Timestamp);
+          if (!Number.isInteger(ts)) return "NaN";
+          var now = Date.now() / 1000;
+          var then = ts/1000;
+          var diff = now - then;
+          var minutes = diff / 60;
+          if (minutes < 1) return ("Just Now");
+          if (minutes < 60) return (Math.round(minutes) + " Minutes Ago");
+          var hours = minutes / 60;
+          if (hours < 24) return (Math.round(hours) + " Hours Ago");
+          var days = hours / 24;
+          return (Math.round(days) + " Days Ago");
+        },
+      }
 }
 </script>
