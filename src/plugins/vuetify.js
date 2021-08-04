710119fc4a3f3f5e9c2a9112445537e4e5bac024
Kaleb Ercanbrack <twitcherc@gmail.com>
2021-08-04 14:53:03 -0600

    optimize vuetify with vuetify-loader

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-
+import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 import colors from 'vuetify/lib/util/colors'
