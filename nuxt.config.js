26b31f4644d85bcddcfb3137ce1470dbbeef2307
Kaleb Ercanbrack <twitcherc@gmail.com>
2021-09-15 23:11:00 -0600

    fix SPA setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  ssr: process.env.SPA,
+  ssr: !process.env.SPA,
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
